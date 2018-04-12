475205eceb4e326fca2b3684eac9129c021dc9ca
James <james.brace@mail.mcgill.ca>
2018-04-12 18:42:05 -0400

    Update package.json for proper build + update gitignore

--- squeezenet/javascript/download-cifar.sh
+++ squeezenet/javascript/download-cifar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ ! -d "./data" ]; then
-    wget "https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"
+    wget "https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz" --no-check-certificate
     node converter.js
 fi
 
