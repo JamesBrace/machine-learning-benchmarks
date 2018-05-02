83d7db9b44ad1fe7086a3b51995f7d5b5d184127
James <james.brace@mail.mcgill.ca>
2018-05-02 10:07:04 -0400

    Run benchmarks on mac

--- benchmarking/run-alex.sh
+++ benchmarking/run-alex.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-yarn benchmark -p Ubuntu -l true -b gpu -t squeezenet
-yarn benchmark -p Ubuntu -l true -b cpu -i 1
+yarn benchmark -t mnist -e python -b cpu -l true -p macbook -i 10
+yarn benchmark -t mnist -e chrome -b cpu -l true -p macbook -i 1
+yarn benchmark -t mnist -e firefox -b cpu -l true -p macbook -i 1
+
+
+
